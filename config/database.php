329eaea5a3495642b730fc9b12fd0d3511df0263
unknown <golya-1996@mail.ru>
2022-10-19 15:11:45 +0500

    bug fix

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@
 const  DATABASE = [
     'host' => 'localhost',
     'port' => 3306,
-    'dbname' => 'know_ledge_city',
+    'dbname' => 'knowledge_city',
     'username' => 'root',
     'password' => ''
 ];
